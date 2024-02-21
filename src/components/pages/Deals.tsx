80b47bc78abf9c4977940afc5146e4e35568f96c
prathaban-s <prathaban@codepalette.ai>
2024-02-21 12:13:17 +0530

    Fix heights and widths

--- src/components/pages/Deals.tsx
+++ src/components/pages/Deals.tsx
@@ -5,9 +5,9 @@ export default function Deals() {
     <div className="h-auto bg-secondary md:h-screen flex justify-center items-center m-auto flex-col gap-7 p-10 ">
       <p className="text-white font-semibold text-5xl"> Get your best deal </p>
       <div className="flex flex-wrap max-w-[1200px] items-start gap-7">
-        <div className="h-[640px] w-[360px] bg-white p-10 rounded-lg"></div>
-        <div className="h-[640px] w-[360px] bg-primary p-10 rounded-lg"></div>
-        <div className="h-[640px] w-[360px] bg-white p-10 rounded-lg"></div>
+        <div className="h-[640px] w-auto sm:w-[360px] bg-white p-10 rounded-lg"></div>
+        <div className="h-[640px] w-auto sm:w-[360px] bg-primary p-10 rounded-lg"></div>
+        <div className="h-[640px] w-auto sm:w-[360px] bg-white p-10 rounded-lg"></div>
       </div>
     </div>
   );
