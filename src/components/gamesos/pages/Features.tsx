b913c06869fdddc5c379ba2159ad27bde1829682
prathaban-s <prathaban@codepalette.ai>
2024-02-22 15:05:52 +0530

    Minor change

--- src/components/gamesos/pages/Features.tsx
+++ src/components/gamesos/pages/Features.tsx
@@ -9,7 +9,7 @@ import {
 export default function Features() {
   return (
     <div className="h-screen bg-cover bg-center features-bg flex flex-col items-end">
-      <div className="flex w-full lg:w-1/2 items-center lg:items-start justify-center h-full flex-col p-10">
+      <div className="flex w-full lg:w-2/5 items-center lg:items-start justify-center h-full flex-col p-10">
         <p className="uppercase text-white text-xl font-bold">
           what's so special?
         </p>
